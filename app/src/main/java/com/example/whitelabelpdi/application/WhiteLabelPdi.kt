dd029c13e3fe6bf371966e4fa6314718a6df8370
Laryssa Fagundes <laryssa.fagundes@usemobile.xyz>
2022-09-21 14:49:23 -0300

    Config hilt application

---
+++ app/src/main/java/com/example/whitelabelpdi/application/WhiteLabelPdi.kt
@@ -0,0 +1,8 @@
+package com.example.whitelabelpdi.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WhiteLabelPdi: Application() {
+}
